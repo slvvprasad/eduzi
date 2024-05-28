d7da848cf168392989719f289214293f1df11f90
slvvprasad <slvvprasad@gmail.com>
2024-05-28 22:53:20 +0530

    educators added,made responsive,cloud images added

--- src/Educators.js
+++ src/Educators.js
@@ -31,17 +31,37 @@ export default function App() {
             ></div>
             <div className="avatar mx-auto bg-white">
               <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
+                src="https://eduzi.s3.amazonaws.com/krishnagopal.jpg"
                 className="rounded-circle img-fluid"
               />
             </div>
             <MDBCardBody>
-              <h4 className="mb-4">Maria Smantha</h4>
+              <h4 className="mb-4"><a href="https://www.linkedin.com/in/krishna-gopal-srinivasa-142393a7" target="_blank" rel="noopener noreferrer">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "1px"
+      }}
+    >
+      <img
+        src="https://cdn-icons-png.flaticon.com/256/174/174857.png"
+        className="company-img"
+        loading="lazy"
+        alt="Bandhan Bank Logo"
+        width="40px"
+      />
+    </div>
+  </a>Krishna Gopal</h4>
+              <h4 className="mb-4">Total experience – 15+ years.<br/>
+Companies - Kotak Securities, CRISIL, Axis Bank, IDFC First Bank, HSBC.</h4>
+              
               <hr />
               <p className="dark-grey-text mt-4">
                 <MDBIcon fas icon="quote-left" className="pe-2" />
-                Lorem ipsum dolor sit amet eos adipisci, consectetur adipisicing
-                elit.
+                With an illustrious career in Finance and Banking of over 15 years, Krishna is one of the main pillars of this program’s success. His career spanned over multiple strategic and leadership roles in Indian and International organizations like CRISIL, Axis Bank, IDFC Bank, HSBC, etc.
+
               </p>
             </MDBCardBody>
           </MDBCard>
@@ -54,17 +74,35 @@ export default function App() {
             ></div>
             <div className="avatar mx-auto bg-white">
               <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
+                src="https://eduzi.s3.amazonaws.com/satyasharma.jpg"
                 className="rounded-circle img-fluid"
               />
             </div>
             <MDBCardBody>
-              <h4 className="mb-4">Lisa Cudrow</h4>
+              <h4 className="mb-4"><a href="https://www.linkedin.com/in/satya-sharma-nittala-frm-474411108" target="_blank" rel="noopener noreferrer">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "1px"
+      }}
+    >
+      <img
+        src="https://cdn-icons-png.flaticon.com/256/174/174857.png"
+        className="company-img"
+        loading="lazy"
+        alt="Bandhan Bank Logo"
+        width="40px"
+      />
+    </div>
+  </a>Satya Sharma</h4>
+              <h4 className="mb-4">Total experience – 15+ years<br/>
+Companies – Bank of Baroda, CRISIL, HSBC.</h4>
               <hr />
               <p className="dark-grey-text mt-4">
                 <MDBIcon fas icon="quote-left" className="pe-2" />
-                Neque cupiditate assumenda in maiores repudi mollitia
-                architecto.
+                A true and a passionate credit analyst, Satya Sharma comes with a long-standing experience in credit and Finance. He has over 15+ years in various domestic rating agencies, banks and MNCs. In his current role as Vice President with a major global bank, Mr. Satya heads the under-writing team for the North American market. He is an FRM fellow and an MBA from IMT Ghaziabad.
               </p>
             </MDBCardBody>
           </MDBCard>
@@ -77,17 +115,37 @@ export default function App() {
             ></div>
             <div className="avatar mx-auto bg-white">
               <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
+                src="https://eduzi.s3.amazonaws.com/aditya.jpg"
                 className="rounded-circle img-fluid"
               />
             </div>
             <MDBCardBody>
-              <h4 className="mb-4">John Smith</h4>
+              <h4 className="mb-4"><a href="https://www.linkedin.com/in/aditya-gupta-18053464" target="_blank" rel="noopener noreferrer">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "1px"
+      }}
+    >
+      <img
+        src="https://cdn-icons-png.flaticon.com/256/174/174857.png"
+        className="company-img"
+        loading="lazy"
+        alt="Bandhan Bank Logo"
+        width="40px"
+      />
+    </div>
+  </a>Aditya Gupta</h4>
+              <h4 className="mb-4">Total experience – 16+ years<br/>
+Companies - HDFC bank, ICICI bank, Kotak Mahindra Bank, IDFC First Bank.
+</h4>
               <hr />
               <p className="dark-grey-text mt-4">
                 <MDBIcon fas icon="quote-left" className="pe-2" />
-                Delectus impedit saepe officiis ab aliquam repellat rem unde
-                ducimus.
+                A seasoned banker, Aditya Gupta is one of our distinguished mentors. He has more than 16 years of experience in sales and credit with his career spanning major Indian banks including HDFC Bank, ICICI Bank, Kotak Mahindra Bank and IDFC First Bank. He is currently Vice President with IDFC First Bank.
+
               </p>
             </MDBCardBody>
           </MDBCard>
@@ -99,21 +157,40 @@ export default function App() {
           <MDBCard className="testimonial-card">
             <div
               className="card-up"
-              style={{ backgroundColor: "#9d789b" }}
+              style={{ backgroundColor: "#9d767b" }}
             ></div>
             <div className="avatar mx-auto bg-white">
               <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
+                src="https://eduzi.s3.amazonaws.com/sushama.jpg"
                 className="rounded-circle img-fluid"
               />
             </div>
             <MDBCardBody>
-              <h4 className="mb-4">Maria Smantha</h4>
+              <h4 className="mb-4"> <a href="https://www.linkedin.com/in/sushama-kambhampati-7a71b1a9" target="_blank" rel="noopener noreferrer">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "1px"
+      }}
+    >
+      <img
+        src="https://cdn-icons-png.flaticon.com/256/174/174857.png"
+        className="company-img"
+        loading="lazy"
+        alt="Bandhan Bank Logo"
+        width="40px"
+      />
+    </div>
+  </a>Sushama K</h4>
+              <h4 className="mb-4">Total experience – 15+ years<br/>
+Companies – Ernst & Young (E&Y), CRISIL.</h4>
               <hr />
               <p className="dark-grey-text mt-4">
                 <MDBIcon fas icon="quote-left" className="pe-2" />
-                Lorem ipsum dolor sit amet eos adipisci, consectetur adipisicing
-                elit.
+                Sushama K is a dynamic and highly qualified mentor with nearly 15 years of professional experience in Finance and Consulting. After her Engineering from Vasavi College of Engineering, Hyderabad, she did her MBA from the reputed T A Pai Management Institute, Manipal (TAPMI)before embarking on her illustrious career. As an Associate Director in CRISIL, she handled the real estate and infrastructure sector in Credit Ratings and achieved great professional success.
+
               </p>
             </MDBCardBody>
           </MDBCard>
@@ -122,21 +199,41 @@ export default function App() {
           <MDBCard className="testimonial-card">
             <div
               className="card-up"
-              style={{ backgroundColor: "#7a81a8" }}
+              style={{ backgroundColor: "#9d897b" }}
             ></div>
             <div className="avatar mx-auto bg-white">
               <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
+                src="https://eduzi.s3.amazonaws.com/sandesh.jpg"
                 className="rounded-circle img-fluid"
               />
             </div>
             <MDBCardBody>
-              <h4 className="mb-4">Lisa Cudrow</h4>
+              <h4 className="mb-4"><a href="https://www.linkedin.com/in/sandesh-gangapatnam-78689b37" target="_blank" rel="noopener noreferrer">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "1px"
+      }}
+    >
+      <img
+        src="https://cdn-icons-png.flaticon.com/256/174/174857.png"
+        className="company-img"
+        loading="lazy"
+        alt="Bandhan Bank Logo"
+        
+        width="40px"
+      />
+    </div>
+  </a><br/>Sandesh Gangapatnam</h4>
+              <h4 className="mb-4">Total experience – 12+ years<br/>
+Companies – CRISIL, HDFC Bank, Citi Bank.</h4>
               <hr />
               <p className="dark-grey-text mt-4">
                 <MDBIcon fas icon="quote-left" className="pe-2" />
-                Neque cupiditate assumenda in maiores repudi mollitia
-                architecto.
+                Sandesh is the most dynamic of all, a very fast-talking, and a compassionate human being. After his MBA from IIT Chennai, he has embarked on his career with CRISIL as a rating analyst and moved his way up to a team leader. He later shifted to sales and marketing and served as a very successfulrelationship manager in HDFC and is currently working as Vice President in Citi Bank.
+
               </p>
             </MDBCardBody>
           </MDBCard>
